2684d6cee874dfaacca122c48c8a503e4fa86735
Eric Tran <eric1tran@gmail.com>
2022-07-27 20:00:41 -0500

    Fix password attribute name in User model

--- tutory_src/models.py
+++ tutory_src/models.py
@@ -11,9 +11,9 @@ class User(db.Model):
     last_name = db.Column(db.String(120))
     files = db.relationship('File', backref='owner', lazy='dynamic')
 
-    def __init__(self, email, password, first_name, last_name):
+    def __init__(self, email, password_hash, first_name, last_name):
         self.email = email
-        self.password = password
+        self.password_hash = password_hash
         self.first_name = first_name
         self.last_name = last_name
 
