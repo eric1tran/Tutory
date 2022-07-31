37994b5799646fbc81627ed9f0c20635dc1132c8
Eric Tran <eric1tran@gmail.com>
2022-07-30 20:41:10 -0500

    Require login on certain pages and redirect back to it once user logs in

--- tutory_src/app.py
+++ tutory_src/app.py
@@ -3,7 +3,8 @@ from user_forms import LoginForm, RegistrationForm
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager, login_user, current_user, logout_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from datetime import datetime
 
@@ -12,6 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'index'
 
 from models import User, File
 
@@ -29,14 +31,20 @@ def index():
     form = LoginForm()
 
     if form.validate_on_submit():
+        # Search for user
         user = User.query.filter_by(email=form.email.data).first()
 
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('index'))
 
+        # login_user will register the user as logged in (i.e. sets current_user to user)
         login_user(user)
-        return redirect(url_for('home'))
+        next_page = request.args.get('next')
+        # if next_page is not set or url is a relative URL (doesn't have a hostname, only a path), go to home page
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
 
     return render_template('login.html', form=form)
 
@@ -63,11 +71,13 @@ def register():
 
 
 @app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
 
 @app.route('/profile')
+@login_required
 def profile():
     return "My Profile Settings"
 
